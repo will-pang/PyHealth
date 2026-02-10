962f62317576685670548b123baa174d3933c026
William Pang <william.pang@aya.yale.edu>
2026-02-10 05:30:35 -0800

    Update roots

---
+++ will_contribution/utilities.py
@@ -0,0 +1,10 @@
+import shutil
+def delete_cache(cache_directory):
+    for item in os.listdir(cache_directory):
+        item_path = os.path.join(cache_directory, item)
+        if os.path.isfile(item_path) or os.path.islink(item_path):
+            os.unlink(item_path)
+        elif os.path.isdir(item_path):
+            shutil.rmtree(item_path)
+
+    print(f"Cache deleted successfully from: {cache_directory}")
